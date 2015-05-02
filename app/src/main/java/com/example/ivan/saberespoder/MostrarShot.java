a950549f203963fb11bd1d3d0e1ec0be841015b1
ernestoivan22 <ernestoivan22@hotmail.com>
2015-05-01 18:13:36 -0600

    Mostrar shot

--- app/src/main/java/com/example/ivan/saberespoder/MostrarShot.java
+++ app/src/main/java/com/example/ivan/saberespoder/MostrarShot.java
@@ -1,29 +1,46 @@
 package com.example.ivan.saberespoder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Locale;
 
 
 public class MostrarShot extends ActionBarActivity {
     TextView tituloS;
     EditText contenidoS;
+    Context context = this;
+    int posicionShot;
+    String[] titulosList;
+    String[] contenidosList;
+    ImageButton speech;
+    TTSManager ttsManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_shot);
         getSupportActionBar().hide();
-        Log.e("MENSAJE", "CAMBIO DE ACTIVITY");
+
+        //----------------------------Cargar elementos----------------------------
         Intent intent = getIntent();
-        String[] titulosList = intent.getStringArrayExtra("listaTitulos");
-        String[] contenidosList = intent.getStringArrayExtra("listaContenidos");
-        int posicionShot = intent.getIntExtra("positionShot",0);
+        titulosList = intent.getStringArrayExtra("listaTitulos");
+        contenidosList = intent.getStringArrayExtra("listaContenidos");
+        posicionShot = intent.getIntExtra("positionShot",0);
 
         tituloS = (TextView)findViewById(R.id.titulo_Shot_mostrar);
         contenidoS = (EditText)findViewById(R.id.contenido_Shot_mostrar);
@@ -33,6 +50,37 @@ public class MostrarShot extends ActionBarActivity {
         contenidoS.setText(contenidosList[posicionShot],TextView.BufferType.EDITABLE);
         contenidoS.setFocusable(false);
 
+        //-------------------------------Detectar Swipe-----------------------------
+        findViewById(R.id.contenido_Shot_mostrar).setOnTouchListener(new OnSwipeTouchListener(context) {
+            @Override
+            public void onSwipeLeft() {
+                if(posicionShot!=titulosList.length-1){
+                    posicionShot++;
+                }
+                tituloS.setText(titulosList[posicionShot]);
+                contenidoS.setText(contenidosList[posicionShot],TextView.BufferType.EDITABLE);
+            }
+            @Override
+            public void onSwipeRight() {
+                if(posicionShot!=0){
+                    posicionShot--;
+                }
+                tituloS.setText(titulosList[posicionShot]);
+                contenidoS.setText(contenidosList[posicionShot],TextView.BufferType.EDITABLE);
+            }
+        });
+
+        //-------------------------------Realizar Speech------------------------------------
+        ttsManager = new TTSManager();
+        ttsManager.init(this);
+        speech = (ImageButton)findViewById(R.id.btn_speech);
+        speech.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                String text = contenidosList[posicionShot];
+                ttsManager.initQueue(text);
+            }
+        });
 
     }
 
@@ -58,4 +106,12 @@ public class MostrarShot extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        ttsManager.shutDown();
+    }
+
+
 }
