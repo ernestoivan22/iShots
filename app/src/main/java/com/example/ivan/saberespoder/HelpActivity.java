762af6a0d4ec5ffaaf86272dc2e101cb407e2d78
cesaregm7 <cesaregm93@gmail.com>
2015-05-02 20:28:08 -0600

    Sistema de ayuda

--- app/src/main/java/com/example/ivan/saberespoder/HelpActivity.java
+++ app/src/main/java/com/example/ivan/saberespoder/HelpActivity.java
@@ -25,7 +25,7 @@ public class HelpActivity extends ActionBarActivity {
 
         usuarioIS = getIntent().getParcelableExtra("usuario");
         ListView listView1 = (ListView) findViewById(R.id.listView3);
-        String[] opciones  = {"Acerca de","¿Cómo buscar shots usando la voz?","¿Cómo reproducir los shots?"};
+        String[] opciones  = {"Acerca de", "\u00bfC\u00f3mo buscar shots usando la voz?", "\u00bfC\u00f3mo reproducir los shots?"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, opciones);
